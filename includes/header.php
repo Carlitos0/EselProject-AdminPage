e231773cc804bff99a30fb24c4cc6593587eeee4
RodrigoChl <chumpitazr130@gmail.com>
2022-07-24 23:38:38 -0500

    Se añadió un pseudo login

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
     <div class="container-fluid py-3">
-        <a class="navbar-brand text-white fw-bold" href="/gestion_esel-copia/productos.php">Admin Page</a>
+        <a class="navbar-brand text-white fw-bold" href="#">Admin Page</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
---
+++ index.php
@@ -0,0 +1,84 @@
+<?php include("./config/conexion.php") ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Inicio</title>
+    <style>
+        body {
+            background-image: url('img/fondo.jpg');
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 100vh;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['ingresar'])) {
+        $email = $_POST['emausu'];
+        $password = $_POST['pasusu'];
+
+        $query = "SELECT * FROM usuario WHERE emausu = '$email' AND pasusu = '$password' LIMIT 1";
+        $result = mysqli_query($con, $query);
+        if ($email == "admin@esel.com" and $password == "123") {
+            $_SESSION['message']  = "Ingreso Correcto";
+            $_SESSION['message_color']  = "success";
+            echo "<script>
+                setTimeout(function(){
+                    location.href = 'productos.php';
+                },900);
+            </script>";
+        } else {
+            $_SESSION['message']  = "Credenciales Incorrectas";
+            $_SESSION['message_color']  = "danger";
+            /* echo "<script>
+                    setTimeout(fuction(){
+                        location.href = 'productos.php';
+                    },3000);
+                </script>"; */
+        }
+    }
+    ?>
+    <div class="container pt-5">
+        <div class="col-10 col-md-7 col-sm-10 col-lg-4 pt-5 mx-auto">
+            <div class="container p-2 mt-4">
+                <?php if (isset($_SESSION['message'])) { ?>
+                    <div class="alert alert-<?= $_SESSION['message_color'] ?> alert-dismissible fade show" role="alert">
+                        <?= $_SESSION['message'] ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php session_unset();
+                } ?>
+            </div>
+            <div class="card">
+                <div class="card-header text-center bg-success">
+                    <h3 class="text-white fw-bolder text-uppercase">Ingreso</h3>
+                </div>
+                <div class="card body p-4">
+                    <form action="index.php" method="POST">
+                        <div class="form-group ">
+                            <input type="email" placeholder="Ingrese su correo" name="emausu" class="form-control p-2">
+                        </div>
+                        <div class="form-group my-3">
+                            <input type="password" placeholder="Contraseña" name="pasusu" class="form-control p-2">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-secondary text-white w-100 btn-lg" name="ingresar">INGRESAR</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
